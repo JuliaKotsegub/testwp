3fe437ab956c06916f081d886aef36f86ef8f8f6
JuliaKotsegub <109865101+JuliaKotsegub@users.noreply.github.com>
2023-09-10 22:03:58 +0300

    test file

---
+++ wp-content/themes/cleantheme/footer.php
@@ -0,0 +1,66 @@
+<footer class="footer">
+      <div class="footer__content">
+        <div class="container">
+          <div class="footer__inner">
+            <div class="footer__info">
+              <a href="#">
+                <img class="footer__logo" src="images/logo.png" alt="Logo">
+              </a>
+              <a href="tel:+380994484498" class="footer__info-tel">+38 099 448 44 98</a>
+              <a href="mailto:treef.td@gmail.com" class="footer__info-email">treef.td@gmail.com</a>
+            </div>
+            <div class="footer__right">
+              <nav class="footer__menu">
+                <ul class="footer__list">
+                  <li>
+                    <h6 class="footer__social-title">Ресурси</h6>
+                  </li>
+                  <li class="footer__social-item"><a href="" class="footer_social-link">Головна</a></li>
+                  <li class="footer__social-item"><a href="" class="footer_social-link">Послуги</a></li>
+                  <li class="footer__social-item"><a href="" class="footer_social-link">Про нас</a></li>
+                  <li class="footer__social-item"><a href="" class="footer_social-link">Блог</a></li>
+                  <li class="footer__social-item"><a href="" class="footer_social-link">Клінерам</a></li>
+                </ul>
+              </nav>
+              <ul class="footer__list">
+                <li>
+                  <h6 class="footer__social-title">Соціальні мережі</h6>
+                </li>
+                <li class="footer__social-item"><a href="" class="footer_social-link"></a>Instagram</li>
+                <li class="footer__social-item"><a href="" class="footer_social-link"></a>Facebook</li>
+              </ul>
+              <ul class="footer__list">
+                <li>
+                  <h6 class="footer__social-title">Контакти</h6>
+                </li>
+                <li class="footer__social-item">
+                  <a href="mailto:treef.td@gmail.com" class="footer__info-email">treef.td@gmail.com</a>
+                </li>
+                <li class="footer__social-item">
+                  <p class="footer__social-address">м. Львів вул. Кульпарківська, 223</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="footer__copyright">
+          <div class="footer__copyright-copy">© 2023 Quality Cleaning. Всі права захищено</div>
+          <div class="footer__copyright-link">
+            <a href="#!">Політика приватності</a>
+            <a href="#!">Договір публічної оферти</a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+
+
+
+  <script src="js/main.min.js">
+
+  </script>
+</body>
+
+</html>
---
+++ wp-content/themes/cleantheme/header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Quality cleaning</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Manrope:wght@400;500;600;700&family=Raleway:wght@700&display=swap"
+    rel="stylesheet">
+  <link rel="stylesheet" href="css/style.min.css">
+</head>
+
+<body>
+
+  <header class="header">
+    <div class="container">
+      <div class="header__inner">
+        <a href="#">
+          <img class="logo" src="images/logo.png" alt="Logo">
+        </a>
+        <nav class="menu">
+          <ul class="menu__list">
+            <li class="menu__list-item">
+              <a class="menu__list-link" href="index.html">Головна</a>
+            </li>
+            <li class="menu__list-item">
+              <a class="menu__list-link" href="services.html">Послуги</a>
+            </li>
+            <li class="menu__list-item">
+              <a class="menu__list-link" href="about.html">Про нас</a>
+            </li>
+            <li class="menu__list-item">
+              <a class="menu__list-link" href="blog.html">Блог</a>
+            </li>
+            <li class="menu__list-item">
+              <a class="menu__list-link" href="#!">Клінерам</a>
+            </li>
+          </ul>
+        </nav>
+        <nav class="menu">
+          <ul class="menu__list">
+            <li class="menu__list-item">
+              <a class="menu__list-exit" href="#!">Увійти</a>
+            </li>
+            <a class="menu__btn" href="#">
+              <div class="menu__btn-text">Замовити</div>
+            </a>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
